8f84b4ced75a4bc6f27dccd215aab85951fc71b0
jangojanga <shibuyajango0523@gmail.com>
2022-06-22 01:26:55 +0900

    issle6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -33,9 +33,17 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+       #曜日
+
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      jango =(@todays_date + x).wday
+      # wdays[jango]
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans,:week_days =>wdays[jango]      }
       @week_days.push(days)
+     
+
+
     end
 
   end
